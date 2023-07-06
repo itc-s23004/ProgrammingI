3ae422227bb6a8139ace652ae9ee1f6e837e16ab
itc-s23004 <s23004@std.it-college.ac.jp>
2023-07-06 06:30:00 +0000

    aiu

---
+++ CHAPTER02/Q2_5_2.py
@@ -0,0 +1,14 @@
+answer = ''
+a = '￮'
+b = '･'
+
+for i in range(4):
+    for j in range(4):
+        if i == j:
+            answer += a
+        else:
+            answer += b
+    answer += '\n'
+
+print(answer)
+
